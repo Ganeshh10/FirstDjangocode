168a223f5776a3efb51533241632aafc14bc0f9f
Ganeshh10 <117337963+Ganeshh10@users.noreply.github.com>
2022-11-03 12:18:07 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import Ganesh2
+# Create your views here.
+def abhi(request):
+
+    if request.method == 'POST':
+
+        name = request.POST.get("name")
+
+        address = request.POST.get("address")
+
+        empID = request.POST.get("empID")
+
+        staffid = request.POST.get("staffid")
+
+        a = Ganesh2(name,address,empID,staffid)
+
+        a.save()
+
+        print(name)
+
+        print(address)
+
+        print(empID)
+
+        print(staffid)
+
+    else:
+
+        return render(request,"data.html",context={})
+
+    return HttpResponse("printed on Console, please Check!!")
